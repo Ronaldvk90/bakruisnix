417a0e818704e2df1fd1006f72dbb93f53ba0308
Ronald van Kouwen <ronaldvk90@outlook.com>
2026-06-07 15:32:04 +0200

    first actual commit. ALL is in development

---
+++ dbus.nix
@@ -0,0 +1,63 @@
+{ system ? builtins.currentSystem }:
+
+let
+
+  pkgs =
+    if system == "armv7l-linux"
+    then
+      (import <nixpkgs> {
+        system = "x86_64-linux";
+      }).pkgsCross.armv7l-hf-multiplatform
+  else import <nixpkgs> {
+    inherit system;
+  }; 
+in
+
+######## DBUS container ##########
+pkgs.dockerTools.buildImage {
+  name = "dbusnix";
+  tag = "latest";
+  copyToRoot = pkgs.buildEnv {
+    ignoreCollisions = true;
+    name = "rootfs";
+    pathsToLink = ["/bin"
+                              "/etc"
+                             ];
+
+  paths = with pkgs; [
+    (pkgs.dbus.overrideAttrs (old: {
+    postInstall = ''
+    rm -rf $out/etc/dbus-1
+  ''; }))
+  procps
+
+  (pkgs.writeTextDir "etc/dbus-1/system.conf" ''
+    <!DOCTYPE  busconfig PUBLIC "-//freedesktop//DTD D-Bus Bus Configuration 1.0//EN" http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+    <busconfig>
+      <includedir>system.d</includedir>
+    </busconfig> '')
+
+  (pkgs.writeTextDir "etc/dbus-1/session.conf" ''
+    <!DOCTYPE busconfig PUBLIC "-//freedesktop//DTD D-Bus Bus Configuration 1.0//EN" "http://www.freedesktop.org/standards/dbus/1.0/busconfig.dtd">
+    <busconfig>
+      <includedir>session.d</includedir>
+    </busconfig> '')
+
+  (pkgs.writeShellScriptBin "entrypoint" ''
+      dbus-daemon --session –fork
+      dbus-daemon --system –nofork
+    '')
+    ]; 
+  }; 
+
+  config = {
+  Env = ["PATH=/bin/"];
+  Cmd = [ "entrypoint" ];
+  Healthcheck = {
+    Test = [ "CMD-SHELL" "pgrep dbus-daemon > /dev/null || exit 1" ];
+    Interval = 5 * 1000000000;
+    Timeout = 10 * 1000000000;
+    Retries = 10;
+    };
+  };
+}
