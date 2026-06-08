2e5f010b943d411766d0925ec55076a46b462569
Ronald van Kouwen <ronaldvk90@outlook.com>
2026-06-08 15:01:12 +0200

    .

--- buildall.sh
+++ buildall.sh
@@ -7,3 +7,7 @@ nix build .#dbus-armv7 -o ./dbus/result-armv7
 nix build .#avahi-x86   -o ./avahi/result-x86
 nix build .#avahi-arm64 -o ./avahi/result-arm64
 nix build .#avahi-armv7 -o ./avahi/result-armv7
+
+nix build .#shairport-sync-x86   -o ./shairport-sync/result-x86
+nix build .#shairport-sync-arm64 -o ./shairport-sync/result-arm64
+nix build .#shairport-sync--armv7 -o ./shairport-sync/result-armv7
